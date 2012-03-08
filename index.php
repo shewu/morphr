c4429142bf8fc88ce100c54805b2d0831c5abc70
Sherry Wu <shewu19@gmail.com>
2012-03-07 21:44:07 -0500

    added index and css files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+<title>6.815/6.865 Image Morphr</title>
+<script src=raphael-min.js></script>
+</head>
+<body>
+
+<table width="800" border="0">
+  <tr >
+    <td height ="450" >&nbsp;</td>
+    <td height ="450" >&nbsp;</td>
+  </tr>
+  <tr>
+    <td width=500><p id="seg1">segmentsBefore=numpy.array([</p></td>
+    <td width =400><p id="seg2">segmentsAfter=numpy.array([</p></td>
+  </tr>
+</table>
+
+<p id="debug"></p>
+
+<footer>&copy; 2012 <a href=mailto:shewu19@gmail.com>Sherry Wu</a>.</footer>
+</body>
+</html>
+
